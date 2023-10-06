39aeea59a3ec77e115ee7987f77ad5c82f84ceb6
CyberLady007 <af4640@nyu.edu>
2023-10-06 16:51:07 +0200

    one more try

--- DNSServer.py
+++ DNSServer.py
@@ -7,10 +7,10 @@ from dns.rdtypes.ANY.MX import MX
 from dns.rdtypes.ANY.SOA import SOA
 import dns.rdata
 import socket
-import threading
 import signal
 import os
 import sys
+
 import hashlib
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
@@ -44,103 +44,93 @@ def decrypt_with_aes(encrypted_data, password, salt):
     return decrypted_data.decode('utf-8')
 
 # Encryption parameters
-
 salt = b'Tandon'  # Encode the salt as a byte-object
 password = 'af4640@nyu.edu'
 input_string = "AlwaysWatching"
 
 # Test encryption and decryption
-
 encrypted_value = encrypt_with_aes(input_string, password, salt)
 decrypted_value = decrypt_with_aes(encrypted_value, password, salt)
 
-# Dictionary containing DNS records
-
+# A dictionary containing DNS records mapping hostnames to different types of DNS data.
 dns_records = {
-    'safebank.com.': '192.168.1.102',
-    'google.com.': '192.168.1.103',
-    'legitsite.com.': '192.168.1.104',
-    'yahoo.com.': '192.168.1.105',
-    'nyu.edu.': {
-        dns.rdatatype.A: '192.168.1.106',
-        dns.rdatatype.TXT: encrypted_value,  # Use the encrypted data here
-        dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
-        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
-        dns.rdatatype.NS: 'ns1.nyu.edu.'
+    'example.com.': {
+        dns.rdatatype.A: '192.168.1.101',
+        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
+        dns.rdatatype.MX: [(10, 'mail.example.com.')],  # List of (preference, mail server) tuples
+        dns.rdatatype.CNAME: 'www.example.com.',
+        dns.rdatatype.NS: 'ns.example.com.',
+        dns.rdatatype.TXT: ('This is a TXT record',),
+        dns.rdatatype.MX: [(10, 'mail.example.com.')],
+        dns.rdatatype.SOA: (
+            'ns1.example.com.', #mname
+            'admin.example.com.', #rname
+            2023081401, #serial
+            3600, #refresh
+            1800, #retry
+            604800, #expire
+            86400, #minimum
+        ),
     },
+   
+    # Add more records as needed
 }
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind(('localhost', 53))  # Bind to localhost and DNS port (53)
-
 def run_dns_server():
-    while True:
-        try:
-            # Wait for incoming DNS requests
-            data, addr = server_socket.recvfrom(1024)
+    # Create a UDP socket and bind it to the local IP address and port (the standard port for DNS)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind(('localhost', 53))  # Bind to localhost and DNS port (53)
 
-            # Parse the request using the `dns.message.from_wire` method
-            request = dns.message.from_wire(data)
+    try:
+        # Wait for incoming DNS requests
+        data, addr = server_socket.recvfrom(1024)
+        # Parse the request using the `dns.message.from_wire` method
+        request = dns.message.from_wire(data)
+        # Create a response message using the `dns.message.make_response` method
+        response = dns.message.make_response(request)
 
-            # Create a response message using the `dns.message.make_response` method
-            response = dns.message.make_response(request)
+        # Get the question from the request
+        question = request.question[0]
+        qname = question.name.to_text()
+        qtype = question.rdtype
 
-            # Get the first question from the request
-            if not request.question:
-                continue  # Ignore invalid queries with no questions
-            
-            question = request.question[0]
-            qname = question.name.to_text()
-            qtype = question.rdtype
+        # Check if there is a record in the `dns_records` dictionary that matches the question
+        if qname in dns_records and qtype in dns_records[qname]:
+            # Retrieve the data for the record and create an appropriate `rdata` object for it
+            answer_data = dns_records[qname][qtype]
 
-            # Check if there is a record in the `dns_records` dictionary that matches the question
-            if qname in dns_records and qtype in dns_records[qname]:
-                # Retrieve the data for the record and create an appropriate `rdata` object for it
-                answer_data = dns_records[qname][qtype]
+            rdata_list = []
 
-                rdata_list = []
-
-                if qtype == dns.rdatatype.MX:
-                    for pref, server in answer_data:
-                        rdata_list.append(MX(dns.rdataclass.IN, dns.rdatatype.MX, pref, server))
-                elif qtype == dns.rdatatype.SOA:
-                    mname, rname, serial, refresh, retry, expire, minimum = answer_data
-                    rdata = SOA(
-                        dns.rdataclass.IN,
-                        dns.rdatatype.SOA,
-                        mname, rname, serial, refresh, retry, expire, minimum
-                    )
-                    rdata_list.append(rdata)
-                else:
-                    if isinstance(answer_data, str):
-                        rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, answer_data)]
-                    else:
-                        rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, data) for data in answer_data]
-                
-                for rdata in rdata_list:
-                    response.answer.append(dns.rrset.RRset(question.name, dns.rdataclass.IN, qtype))
-                    response.answer[-1].add(rdata)
-
-                # Set the AA (Authoritative Answer) flag manually
-                response.flags |= dns.flags.AA
-
+            if qtype == dns.rdatatype.MX:
+                for pref, server in answer_data:
+                    rdata_list.append(MX(dns.rdataclass.IN, dns.rdatatype.MX, pref, server))
+            elif qtype == dns.rdatatype.SOA:
+                mname, rname, serial, refresh, retry, expire, minimum = answer_data
+                rdata = SOA(
+                    dns.rdataclass.IN,
+                    dns.rdatatype.SOA,
+                    mname, rname, serial, refresh, retry, expire, minimum
+                )
+                rdata_list.append(rdata)
             else:
-                # Handle queries for hostnames not in the DNS records with an appropriate error response
-                response.set_rcode(dns.rcode.NXDOMAIN)
+                if isinstance(answer_data, str):
+                    rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, answer_data)]
+                else:
+                    rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, data) for data in answer_data]
+            for rdata in rdata_list:
+                response.answer.append(dns.rrset.RRset(question.name, dns.rdataclass.IN, qtype))
+                response.answer[-1].add(rdata)
 
-            # Send the response back to the client
-            server_socket.sendto(response.to_wire(), addr)
-            print("Responding to request:", qname)
+        # Set the response flags (Authoritative Answer)
+        response.flags |= dns.flags.AA
 
-        except KeyboardInterrupt:
-            print('\nExiting...')
-            server_socket.close()
-            sys.exit(0)
-        except Exception as e:
-            print("An error occurred:", str(e))
+        # Send the response back to the client using the sendto method of the socket object
+        server_socket.sendto(response.to_wire(), addr)
+        print("Responding to request:", qname)
+    except KeyboardInterrupt:
+        print('\nExiting...')
+        server_socket.close()
+        sys.exit(0)
 
-# Handle DNS query timeouts and exceptions in the DNS server thread
-try:
+if __name__ == '__main__':
     run_dns_server()
-except Exception as e:
-    print("Exception in DNS server thread:", str(e))
