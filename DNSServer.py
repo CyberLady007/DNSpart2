e344103de2d09149186c14d533e66f395e985736
CyberLady007 <af4640@nyu.edu>
2023-10-06 15:30:54 +0200

    one more try

--- DNSServer.py
+++ DNSServer.py
@@ -11,7 +11,6 @@ import threading
 import signal
 import os
 import sys
-
 import hashlib
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
@@ -20,6 +19,7 @@ import base64
 import ast
 
 # Define Encryption and Decryption Functions:
+
 def generate_aes_key(password, salt):
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
@@ -44,15 +44,18 @@ def decrypt_with_aes(encrypted_data, password, salt):
     return decrypted_data.decode('utf-8')
 
 # Encryption parameters
+
 salt = b'Tandon'  # Encode the salt as a byte-object
-password = 'af4640@nyu.edu' 
+password = 'af4640@nyu.edu'
 input_string = "AlwaysWatching"
 
 # Test encryption and decryption
+
 encrypted_value = encrypt_with_aes(input_string, password, salt)
 decrypted_value = decrypt_with_aes(encrypted_value, password, salt)
 
 # Dictionary containing DNS records
+
 dns_records = {
     'safebank.com.': '192.168.1.102',
     'google.com.': '192.168.1.103',
@@ -65,24 +68,27 @@ dns_records = {
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.NS: 'ns1.nyu.edu.'
     },
-    
 }
 
-def run_dns_server():
-    # Create a UDP socket and bind it to the local IP address and port (the standard port for DNS)
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind(('localhost', 53))  # Bind to localhost and DNS port (53)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(('localhost', 53))  # Bind to localhost and DNS port (53)
 
+def run_dns_server():
     while True:
         try:
             # Wait for incoming DNS requests
             data, addr = server_socket.recvfrom(1024)
+
             # Parse the request using the `dns.message.from_wire` method
             request = dns.message.from_wire(data)
+
             # Create a response message using the `dns.message.make_response` method
             response = dns.message.make_response(request)
 
-            # Get the question from the request
+            # Get the first question from the request
+            if not request.question:
+                continue  # Ignore invalid queries with no questions
+            
             question = request.question[0]
             qname = question.name.to_text()
             qtype = question.rdtype
@@ -110,36 +116,31 @@ def run_dns_server():
                         rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, answer_data)]
                     else:
                         rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, data) for data in answer_data]
+                
                 for rdata in rdata_list:
                     response.answer.append(dns.rrset.RRset(question.name, dns.rdataclass.IN, qtype))
                     response.answer[-1].add(rdata)
 
-            # Set the response flags (Authoritative Answer)
-            response.flags |= dns.flags.AA
+                # Set the AA (Authoritative Answer) flag manually
+                response.flags |= dns.flags.AA
+
+            else:
+                # Handle queries for hostnames not in the DNS records with an appropriate error response
+                response.set_rcode(dns.rcode.NXDOMAIN)
 
             # Send the response back to the client
             server_socket.sendto(response.to_wire(), addr)
             print("Responding to request:", qname)
+
         except KeyboardInterrupt:
             print('\nExiting...')
             server_socket.close()
             sys.exit(0)
+        except Exception as e:
+            print("An error occurred:", str(e))
 
-def run_dns_server_user():
-    print("Input 'q' and hit 'enter' to quit")
-    print("DNS server is running...")
-
-    def user_input():
-        while True:
-            cmd = input()
-            if cmd.lower() == 'q':
-                print('Quitting...')
-                os.kill(os.getpid(), signal.SIGINT)
-
-    input_thread = threading.Thread(target=user_input)
-    input_thread.daemon = True
-    input_thread.start()
+# Handle DNS query timeouts and exceptions in the DNS server thread
+try:
     run_dns_server()
-
-if __name__ == '__main__':
-    run_dns_server_user()
+except Exception as e:
+    print("Exception in DNS server thread:", str(e))
