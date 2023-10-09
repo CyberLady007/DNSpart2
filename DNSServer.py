ace5ee94f114d8e1594e974ca2796165126160f6
CyberLady007 <af4640@nyu.edu>
2023-10-09 18:47:22 +0200

    new aproach

--- DNSServer.py
+++ DNSServer.py
@@ -7,6 +7,7 @@ from dns.rdtypes.ANY.MX import MX
 from dns.rdtypes.ANY.SOA import SOA
 import dns.rdata
 import socket
+import threading
 import signal
 import os
 import sys
@@ -18,8 +19,7 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import base64
 import ast
 
-# Define Encryption and Decryption Functions:
-
+# Function to generate an AES key from a password and salt
 def generate_aes_key(password, salt):
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
@@ -31,106 +31,117 @@ def generate_aes_key(password, salt):
     key = base64.urlsafe_b64encode(key)
     return key
 
+# Function to encrypt data with AES
 def encrypt_with_aes(input_string, password, salt):
     key = generate_aes_key(password, salt)
     f = Fernet(key)
     encrypted_data = f.encrypt(input_string.encode('utf-8'))
     return encrypted_data
 
+# Function to decrypt data with AES
 def decrypt_with_aes(encrypted_data, password, salt):
     key = generate_aes_key(password, salt)
     f = Fernet(key)
     decrypted_data = f.decrypt(encrypted_data)
     return decrypted_data.decode('utf-8')
 
-# Encryption parameters
-salt = b'Tandon'  # Encode the salt as a byte-object
+# Salt and password for encryption
+salt = b'Tandon'  # Remember it should be a byte-object
 password = 'af4640@nyu.edu'
-input_string = "AlwaysWatching"
+input_string = 'AlwaysWatching'
 
-# Test encryption and decryption
-encrypted_value = encrypt_with_aes(input_string, password, salt)
-decrypted_value = decrypt_with_aes(encrypted_value, password, salt)
-
-# A dictionary containing DNS records mapping hostnames to different types of DNS data.
+# Create a dictionary of DNS records
 dns_records = {
-    'example.com.': {
-        dns.rdatatype.A: '192.168.1.101',
-        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
-        dns.rdatatype.MX: [(10, 'mail.example.com.')],  # List of (preference, mail server) tuples
-        dns.rdatatype.CNAME: 'www.example.com.',
-        dns.rdatatype.NS: 'ns.example.com.',
-        dns.rdatatype.TXT: ('This is a TXT record',),
-        dns.rdatatype.MX: [(10, 'mail.example.com.')],
-        dns.rdatatype.SOA: (
-            'ns1.example.com.', #mname
-            'admin.example.com.', #rname
-            2023081401, #serial
-            3600, #refresh
-            1800, #retry
-            604800, #expire
-            86400, #minimum
-        ),
+    'safebank.com.': {
+        dns.rdatatype.A: '192.168.1.102',
     },
-   
-    # Add more records as needed
+    'google.com.': {
+        dns.rdatatype.A: '192.168.1.103',
+    },
+    'legitsite.com.': {
+        dns.rdatatype.A: '192.168.1.104',
+    },
+    'yahoo.com.': {
+        dns.rdatatype.A: '192.168.1.105',
+    },
+    'nyu.edu.': {
+        dns.rdatatype.A: '192.168.1.106',
+        dns.rdatatype.TXT: encrypt_with_aes(input_string, password, salt),  # Encrypt and store the secret data
+        dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
+        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
+        dns.rdatatype.NS: 'ns1.nyu.edu.',
+    },
 }
 
+# Create a UDP socket and bind it to the local IP address and DNS port (53)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(('127.0.0.1', 53))
+
+# Function to run the DNS server
 def run_dns_server():
-    # Create a UDP socket and bind it to the local IP address and port (the standard port for DNS)
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind(('localhost', 53))  # Bind to localhost and DNS port (53)
+    while True:
+        try:
+            # Wait for incoming DNS requests
+            data, addr = server_socket.recvfrom(1024)
+            # Parse the request using the `dns.message.from_wire` method
+            request = dns.message.from_wire(data)
+            # Create a response message using the `dns.message.make_response` method
+            response = dns.message.make_response(request)
 
-    try:
-        # Wait for incoming DNS requests
-        data, addr = server_socket.recvfrom(1024)
-        # Parse the request using the `dns.message.from_wire` method
-        request = dns.message.from_wire(data)
-        # Create a response message using the `dns.message.make_response` method
-        response = dns.message.make_response(request)
+            # Get the question from the request
+            question = request.question[0]
+            qname = question.name.to_text()
+            qtype = question.rdtype
 
-        # Get the question from the request
-        question = request.question[0]
-        qname = question.name.to_text()
-        qtype = question.rdtype
+            # Check if there is a record in the `dns_records` dictionary that matches the question
+            if qname in dns_records and qtype in dns_records[qname]:
+                # Retrieve the data for the record and create an appropriate `rdata` object for it
+                answer_data = dns_records[qname][qtype]
 
-        # Check if there is a record in the `dns_records` dictionary that matches the question
-        if qname in dns_records and qtype in dns_records[qname]:
-            # Retrieve the data for the record and create an appropriate `rdata` object for it
-            answer_data = dns_records[qname][qtype]
+                rdata_list = []
 
-            rdata_list = []
-
-            if qtype == dns.rdatatype.MX:
-                for pref, server in answer_data:
-                    rdata_list.append(MX(dns.rdataclass.IN, dns.rdatatype.MX, pref, server))
-            elif qtype == dns.rdatatype.SOA:
-                mname, rname, serial, refresh, retry, expire, minimum = answer_data
-                rdata = SOA(
-                    dns.rdataclass.IN,
-                    dns.rdatatype.SOA,
-                    mname, rname, serial, refresh, retry, expire, minimum
-                )
-                rdata_list.append(rdata)
-            else:
-                if isinstance(answer_data, str):
-                    rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, answer_data)]
+                if qtype == dns.rdatatype.MX:
+                    for pref, server in answer_data:
+                        rdata_list.append(MX(dns.rdataclass.IN, dns.rdatatype.MX, pref, server))
+                elif qtype == dns.rdatatype.SOA:
+                    mname, rname, serial, refresh, retry, expire, minimum = answer_data
+                    rdata = SOA(dns.rdataclass.IN, dns.rdatatype.SOA, mname, rname, serial, refresh, retry, expire, minimum)
+                    rdata_list.append(rdata)
                 else:
-                    rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, data) for data in answer_data]
-            for rdata in rdata_list:
-                response.answer.append(dns.rrset.RRset(question.name, dns.rdataclass.IN, qtype))
-                response.answer[-1].add(rdata)
+                    if isinstance(answer_data, str):
+                        rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, answer_data)]
+                    else:
+                        rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, data) for data in answer_data]
+                for rdata in rdata_list:
+                    response.answer.append(dns.rrset.RRset(question.name, dns.rdataclass.IN, qtype))
+                    response.answer[-1].add(rdata)
 
-        # Set the response flags (Authoritative Answer)
-        response.flags |= dns.flags.AA
+            # Set the AA (Authoritative Answer) flag manually using bitwise operations
+            response.flags |= 1 << 10
 
-        # Send the response back to the client using the sendto method of the socket object
-        server_socket.sendto(response.to_wire(), addr)
-        print("Responding to request:", qname)
-    except KeyboardInterrupt:
-        print('\nExiting...')
-        server_socket.close()
-        sys.exit(0)
+            # Send the response back to the client using the `server_socket.sendto` method
+            server_socket.sendto(response.to_wire(), addr)
+        except KeyboardInterrupt:
+            print('\nExiting...')
+            server_socket.close()
+            sys.exit(0)
+
+# Function to run the DNS server with user input to quit
+def run_dns_server_user():
+    print("Input 'q' and hit 'enter' to quit")
+    print("DNS server is running...")
+
+    def user_input():
+        while True:
+            cmd = input()
+            if cmd.lower() == 'q':
+                print('Quitting...')
+                os.kill(os.getpid(), signal.SIGINT)
+
+    input_thread = threading.Thread(target=user_input)
+    input_thread.daemon = True
+    input_thread.start()
+    run_dns_server()
 
 if __name__ == '__main__':
-    run_dns_server()
+    run_dns_server_user()
