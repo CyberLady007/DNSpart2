52e770b12a7d13be227a975fd042ae370f3d0b79
CyberLady007 <af4640@nyu.edu>
2023-10-10 19:49:48 +0200

    new aproach

--- DNSServer.py
+++ DNSServer.py
@@ -1,23 +1,18 @@
 import dns.message
 import dns.rdatatype
 import dns.rdataclass
-import dns.rdtypes
-import dns.rdtypes.ANY
 from dns.rdtypes.ANY.MX import MX
-from dns.rdtypes.ANY.SOA import SOA
+from dns.rdataclass import IN
+from dns.rdatatype import MX, SOA, A, AAAA, CNAME, TXT, NS
 import dns.rdata
 import socket
 import threading
 import signal
-import os
 import sys
-
-import hashlib
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import base64
-import ast
 
 def generate_aes_key(password, salt):
     kdf = PBKDF2HMAC(
@@ -46,105 +41,82 @@ def decrypt_with_aes(encrypted_data, password, salt):
         print(f"Decrypt error! Type: {type(e)} Value: {e}")
         return None
 
-# Prepare Encryption Parameters
-salt = b'Tandon'  # Remember it should be a byte-object
+salt = b'Tandon'
 password = 'af4640@nyu.edu'
 secret_data = 'AlwaysWatching'
 
-# Create a dictionary containing DNS records
 dns_records = {
-    'safebank.com.': {
-        dns.rdatatype.A: '192.168.1.102',
-    },
-    'google.com.': {
-        dns.rdatatype.A: '192.168.1.103',
-    },
-    'legitsite.com.': {
-        dns.rdatatype.A: '192.168.1.104',
-    },
-    'yahoo.com.': {
-        dns.rdatatype.A: '192.168.1.105',
-    },
+    'safebank.com.': { A: '192.168.1.102' },
+    'google.com.': { A: '192.168.1.103' },
+    'legitsite.com.': { A: '192.168.1.104' },
+    'yahoo.com.': { A: '192.168.1.105' },
     'nyu.edu.': {
-        dns.rdatatype.A: '192.168.1.106',
-        dns.rdatatype.TXT: encrypt_with_aes(secret_data, password, salt),
-        dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
-        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
-        dns.rdatatype.NS: 'ns1.nyu.edu.',
+        A: '192.168.1.106',
+        TXT: encrypt_with_aes(secret_data, password, salt),
+        MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
+        AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
+        NS: 'ns1.nyu.edu.'
     },
     'example.com.': {
-        dns.rdatatype.A: '192.168.1.101',
-        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
-        dns.rdatatype.MX: [(10, 'mail.example.com.')],
-        dns.rdatatype.CNAME: 'www.example.com.',
-        dns.rdatatype.NS: 'ns.example.com.',
-        dns.rdatatype.TXT: ('This is a TXT record',),
-        dns.rdatatype.MX: [(10, 'mail.example.com.')],
-        dns.rdatatype.SOA: (
-            'ns1.example.com.', #mname
-            'admin.example.com.', #rname
-            2023081401, #serial
-            3600, #refresh
-            1800, #retry
-            604800, #expire
-            86400, #minimum
+        A: '192.168.1.101',
+        AAAA: '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
+        MX: [(10, 'mail.example.com.')],
+        CNAME: 'www.example.com.',
+        NS: 'ns.example.com.',
+        TXT: ('This is a TXT record',),
+        SOA: (
+            'ns1.example.com.',  # mname
+            'admin.example.com.',  # rname
+            2023081401,  # serial
+            3600,  # refresh
+            1800,  # retry
+            604800,  # expire
+            86400,  # minimum
         ),
     },
 }
 
 def run_dns_server():
-    # Create a UDP socket and bind it to the local IP address and port (the standard port for DNS)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind(('127.0.0.1', 53))  # Bind to localhost and port 53 (standard DNS port)
+    server_socket.bind(('127.0.0.1', 53))
 
     while True:
         try:
-            # Wait for incoming DNS requests
             data, addr = server_socket.recvfrom(1024)
-            # Parse the request using the `dns.message.from_wire` method
             request = dns.message.from_wire(data)
-            # Create a response message using the `dns.message.make_response` method
             response = dns.message.make_response(request)
-
-            # Get the question from the request
             question = request.question[0]
             qname = question.name.to_text()
             qtype = question.rdtype
 
-            # Check if there is a record in the `dns_records` dictionary that matches the question
             if qname in dns_records and qtype in dns_records[qname]:
-                # Retrieve the data for the record and create an appropriate `rdata` object for it
                 answer_data = dns_records[qname][qtype]
-
                 rdata_list = []
 
-                if qtype == dns.rdatatype.MX:
+                if qtype == MX:
                     for pref, server in answer_data:
-                        rdata_list.append(MX(dns.rdataclass.IN, dns.rdatatype.MX, pref, server))
-                elif qtype == dns.rdatatype.SOA:
+                        rdata_list.append(MX(IN, MX, pref, server))
+                elif qtype == SOA:
                     mname, rname, serial, refresh, retry, expire, minimum = answer_data
-                    rdata = SOA(dns.rdataclass.IN, dns.rdatatype.SOA, mname, rname, serial, refresh, retry, expire, minimum)
+                    rdata = SOA(IN, SOA, mname, rname, serial, refresh, retry, expire, minimum)
                     rdata_list.append(rdata)
-                else:
+                elif qtype == A or qtype == AAAA or qtype == CNAME or qtype == TXT or qtype == NS:
                     if isinstance(answer_data, str):
-                        rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, answer_data)]
+                        rdata_list = [dns.rdata.from_text(IN, qtype, answer_data)]
                     elif isinstance(answer_data, bytes):
                         decrypted_data = decrypt_with_aes(answer_data, password, salt)
-                        rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, decrypted_data)]
-                    elif isinstance(answer_data, tuple):
-                        rdata_list = [MX(dns.rdataclass.IN, dns.rdatatype.MX, *answer_data)]
+                        rdata_list = [dns.rdata.from_text(IN, qtype, decrypted_data)]
+                    elif isinstance(answer_data, tuple) and qtype == MX:
+                        rdata_list = [MX(IN, MX, *answer_data)]
                     else:
                         print(f"Unexpected data type in answer_data: {answer_data}")
                         raise ValueError("Unexpected data type in answer_data")
 
                 for rdata in rdata_list:
-                    response.answer.append(dns.rrset.RRset(question.name, dns.rdataclass.IN, qtype))
+                    response.answer.append(dns.rrset.RRset(question.name, IN, qtype))
                     response.answer[-1].add(rdata)
 
-            # Set the AA (Authoritative Answer) flag manually
             response.flags |= 1 << 10
-
-            # Send the response back to the client using the `server_socket.sendto` method
             server_socket.sendto(response.to_wire(), addr)
             print("Responding to request:", qname)
         except KeyboardInterrupt:
