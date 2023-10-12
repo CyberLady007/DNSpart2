0d14853172245a39441c0e5c6b942f60a2596f69
CyberLady007 <af4640@nyu.edu>
2023-10-12 15:53:20 +0200

    new aproach

--- DNSServer.py
+++ DNSServer.py
@@ -9,7 +9,9 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import Fernet
 import base64
+import sys
 
+# Function to generate AES key
 def generate_aes_key(password, salt):
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
@@ -20,17 +22,19 @@ def generate_aes_key(password, salt):
     key = kdf.derive(password.encode('utf-8'))
     key = base64.urlsafe_b64encode(key)
     return key
-    
+
+# Function to encrypt data with AES
 def encrypt_with_aes(input_string, password, salt):
     key = generate_aes_key(password, salt)
     f = Fernet(key)
     encrypted_data = f.encrypt(input_string.encode('utf-8'))
-    return encrypted_data    
+    return encrypted_data
 
+# Function to decrypt data with AES
 def decrypt_with_aes(encrypted_data, password, salt):
     key = generate_aes_key(password, salt)
     f = Fernet(key)
-    decrypted_data = f.decrypt(encrypted_data.decode)
+    decrypted_data = f.decrypt(encrypted_data)
     return decrypted_data.decode('utf-8')
 
 # Prepare Encryption Parameters
@@ -66,7 +70,6 @@ dns_records = {
         dns.rdatatype.CNAME: 'www.example.com.',
         dns.rdatatype.NS: 'ns.example.com.',
         dns.rdatatype.TXT: ('This is a TXT record',),
-        dns.rdatatype.MX: [(10, 'mail.example.com.')],
         dns.rdatatype.SOA: (
             'ns1.example.com.', #mname
             'admin.example.com.', #rname
@@ -80,29 +83,20 @@ dns_records = {
 }
 
 def run_dns_server():
-    # Create a UDP socket and bind it to the local IP address and port (the standard port for DNS)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind(('127.0.0.1', 53))  # Bind to localhost and port 53 (standard DNS port)
+    server_socket.bind(('127.0.0.1', 53))
 
     while True:
         try:
-            # Wait for incoming DNS requests
             data, addr = server_socket.recvfrom(1024)
-            # Parse the request using the `dns.message.from_wire` method
             request = dns.message.from_wire(data)
-            # Create a response message using the `dns.message.make_response` method
             response = dns.message.make_response(request)
-
-            # Get the question from the request
             question = request.question[0]
             qname = question.name.to_text()
             qtype = question.rdtype
 
-            # Check if there is a record in the `dns_records` dictionary that matches the question
             if qname in dns_records and qtype in dns_records[qname]:
-                # Retrieve the data for the record and create an appropriate `rdata` object for it
                 answer_data = dns_records[qname][qtype]
-
                 rdata_list = []
 
                 if qtype == dns.rdatatype.MX:
@@ -118,11 +112,6 @@ def run_dns_server():
                     elif isinstance(answer_data, bytes):
                         decrypted_data = decrypt_with_aes(answer_data, password, salt)
                         rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, decrypted_data)]
-                    elif isinstance(answer_data, tuple):
-                        rdata_list = [MX(dns.rdataclass.IN, dns.rdatatype.MX, *answer_data)]
-                    else:
-                        print(f"Unexpected data type in answer_data: {answer_data}")
-                        raise ValueError("Unexpected data type in answer_data")
 
                 for rdata in rdata_list:
                     response.answer.append(dns.rrset.RRset(question.name, dns.rdataclass.IN, qtype))
@@ -134,6 +123,7 @@ def run_dns_server():
             # Send the response back to the client using the `server_socket.sendto` method
             server_socket.sendto(response.to_wire(), addr)
             print("Responding to request:", qname)
+
         except KeyboardInterrupt:
             print('\nExiting...')
             server_socket.close()
