60ec0e11261205877d2ff9a47fedb1f8576d20d3
CyberLady007 <af4640@nyu.edu>
2023-10-17 14:14:36 +0200

    new aproach

--- DNSServer.py
+++ DNSServer.py
@@ -81,7 +81,7 @@ password = 'af4640@nyu.edu'
 secret_data = 'AlwaysWatching'
 
 
-encrypted_value = encrypt_with_aes(input_string, password, salt) # test function
+encrypted_value = encrypt_with_aes(secret_data, password, salt) # test function
 decrypted_value = decrypt_with_aes(encrypted_value, password, salt)  # test function
 
 
