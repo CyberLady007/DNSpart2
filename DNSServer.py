8385877fa03edc355001188c1199ef89897d6570
CyberLady007 <af4640@nyu.edu>
2023-10-18 00:39:15 +0200

    new aproach

--- DNSServer.py
+++ DNSServer.py
@@ -1,6 +1,8 @@
 import dns.message
 import dns.rdatatype
 import dns.rdataclass
+import dns.rdtypes
+import dns.rdtypes.ANY
 from dns.rdtypes.ANY.MX import MX
 from dns.rdtypes.ANY.SOA import SOA
 import dns.rdata
@@ -9,10 +11,14 @@ import threading
 import signal
 import os
 import sys
+import hashlib
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import base64
+import ast
+import hashlib
+
 
 # Set encryption parameters
 salt = b'Tandon'
@@ -43,9 +49,13 @@ def decrypt_with_aes(encrypted_data, password, salt):
     return decrypted_data.decode('utf-8')
 
 # Encrypt the secret data
-encrypted_data = encrypt_with_aes(input_string, password, salt)
-decrypted_value = decrypt_with_aes(encrypted_data, password, salt)
+encrypted_value = encrypt_with_aes(input_string, password, salt) # test function
+decrypted_value = decrypt_with_aes(encrypted_value, password, salt)  # test function
 
+def generate_sha256_hash(input_string):
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(input_string.encode('utf-8'))
+    return sha256_hash.hexdigest()
 
 # Define DNS records including an exfiltration record
 dns_records = {
@@ -62,11 +72,11 @@ dns_records = {
         dns.rdatatype.A: '192.168.1.105',
     },
     'nyu.edu.': {
-        dns.rdatatype.A: '192.168.1.106',
-        dns.rdatatype.TXT: encrypted_data,  # Store the encrypted data as bytes
-        dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
-        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
-        dns.rdatatype.NS: 'ns1.nyu.edu.',
+         dns.rdatatype.A: '192.168.1.106',
+         dns.rdatatype.TXT: encrypted_value,  # Store the encrypted data as bytes
+         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
+         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
+         dns.rdatatype.NS: 'ns1.nyu.edu.',
     },
     'example.com.': {
         dns.rdatatype.A: '192.168.1.101',
@@ -90,6 +100,7 @@ dns_records = {
 def run_dns_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind(('127.0.0.1', 53))
+
     while True:
         try:
             data, addr = server_socket.recvfrom(1024)
@@ -99,8 +110,10 @@ def run_dns_server():
             qname = question.name.to_text()
             qtype = question.rdtype
             rdata_list = []
+
             if qname in dns_records and qtype in dns_records[qname]:
                 answer_data = dns_records[qname][qtype]
+
                 if qtype == dns.rdatatype.MX:
                     for pref, server in answer_data:
                         rdata_list.append(MX(dns.rdataclass.IN, dns.rdatatype.MX, pref, server))
@@ -116,9 +129,13 @@ def run_dns_server():
                 else:
                     if isinstance(answer_data, str):
                         rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, answer_data)]
+                    else:
+                        rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, data) for data in answer_data]
+
                 for rdata in rdata_list:
                     response.answer.append(dns.rrset.RRset(question.name, dns.rdataclass.IN, qtype))
                     response.answer[-1].add(rdata)
+
             response.flags |= 1 << 10
             server_socket.sendto(response.to_wire(), addr)
             print("Responding to request:", qname)
@@ -130,12 +147,14 @@ def run_dns_server():
 def run_dns_server_user():
     print("Input 'q' and hit 'enter' to quit")
     print("DNS server is running...")
+
     def user_input():
         while True:
             cmd = input()
             if cmd.lower() == 'q':
                 print('Quitting...')
                 os.kill(os.getpid(), signal.SIGINT)
+
     input_thread = threading.Thread(target=user_input)
     input_thread.daemon = True
     input_thread.start()
